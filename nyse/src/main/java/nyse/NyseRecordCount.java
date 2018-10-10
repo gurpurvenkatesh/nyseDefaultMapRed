6427291e63ad5d8f1f061219a4352df30e0ac4f3
Venkatesh Gurupura Pai <gurpurvenkatesh@gmail.com>
2018-10-10 06:14:56 +0530

    Comitting the nyse changes to Git.

---
+++ nyse/src/main/java/nyse/NyseRecordCount.java
@@ -0,0 +1,41 @@
+package nyse;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class NyseRecordCount extends Configured implements Tool {
+
+	@Override
+	public int run(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Job job = Job.getInstance(getConf());
+
+		job.setMapperClass(Mapper.class);				
+		job.setReducerClass(Reducer.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		System.exit(ToolRunner.run(new NyseRecordCount(), args));
+	}
+
+}
